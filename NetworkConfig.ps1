008c88643ff6718ee5a2d6cebba5212a39998f28
Matthew Earles <142598248+Matt01965@users.noreply.github.com>
2023-12-18 13:07:41 -0800

    Update NetworkConfig.ps1

--- NetworkConfig.ps1
+++ NetworkConfig.ps1
@@ -20,3 +20,18 @@ $adapter | New-NetIPAddress `
  -DefaultGateway $Gateway
 # Configure the DNS client server IP addresses
 $adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
+
+
+$IPType = "IPv4"
+$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+$interface = $adapter | Get-NetIPInterface -AddressFamily $IPType
+If ($interface.Dhcp -eq "Disabled") {
+ # Remove existing gateway
+ If (($interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $interface | Remove-NetRoute -Confirm:$false
+ }
+ # Enable DHCP
+ $interface | Set-NetIPInterface -DHCP Enabled
+ # Configure the DNS Servers automatically
+ $interface | Set-DnsClientServerAddress -ResetServerAddresses
+}
